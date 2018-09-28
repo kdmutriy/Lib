c0858c83274f3a242686b08f42da9a9e5a41cce3
K Dmitriy <k.dmutriy@gmail.com>
2018-09-28 21:59:41 +0300

    Model

--- Lib/Models/Author.cs
+++ Lib/Models/Author.cs
@@ -7,5 +7,12 @@ namespace Lib.Models
 {
     public class Author
     {
+        public int Id { get; set; }
+        public string NameAuthor { get; set; }
+        public ICollection<Book> Books { get; set; }
+        public Author()
+        {
+            Books = new List<Book>();
+        }
     }
 }
--- Lib/Models/Book.cs
+++ Lib/Models/Book.cs
@@ -7,5 +7,12 @@ namespace Lib.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string NameBook { get; set; }
+        public ICollection<Author> Authors { get; set; }
+        public Book()
+        {
+            Authors = new List<Author>();
+        }
     }
 }
